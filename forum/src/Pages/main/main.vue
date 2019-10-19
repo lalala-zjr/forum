a4a58d421a5b464a1e049d3c303c3b915d28b81c
lalala-zjr <1365351516@qq.com>
2019-10-19 17:35:36 +0800

    初始化

---
+++ forum/src/Pages/main/main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div add="main">
+        <p>啦啦啦</p>
+    </div>
+</template>
+<script>
+export default{
+  data () {
+    return {
+      a: 10
+    }
+  }
+}
+</script>
+<style scored>
+
+</style>
