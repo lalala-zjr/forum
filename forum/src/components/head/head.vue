5126415cbdf681556f831c00501c821fc2c73362
lalala-zjr <1365351516@qq.com>
2019-10-19 18:05:55 +0800

    头部导航

---
+++ forum/src/components/head/head.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="head"></div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      a: 10
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
--- forum/src/router/index.js
+++ forum/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import main from '../Pages/main/main.vue'
+import head from '../components/head/head.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'main',
       component: main
+    },
+    {
+      path: '/head',
+      name: 'head',
+      component: head
     }
   ]
 })
