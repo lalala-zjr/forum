4c92ab61613cb0c193be4a7e877169774276d4b2
lalala-zjr <1365351516@qq.com>
2019-10-20 16:01:33 +0800

    首页右侧

--- forum/src/Pages/main/right/right.vue
+++ forum/src/Pages/main/right/right.vue
@@ -1,14 +1,25 @@
 <template>
    <div id="right">
        <div class="word">{{word}}</div>
-       <div class="w1">排行榜</div>
+       <div class="w1">{{word1}}</div>
+       <div class="w2">{{word2}}</div>
+       <div class="w3">
+           <div class="day">{{d1}}</div>
+           <div class="week">{{d2}}</div>
+           <div class="month">{{d3}}</div>
+       </div>
    </div>
 </template>
 <script>
 export default{
   data () {
     return {
-      word: '发帖'
+      word: '发帖',
+      word1: '排行榜',
+      word2: '热搜排名',
+      d1: '今日',
+      d2: '本周',
+      d3: '本月'
     }
   }
 }
@@ -41,12 +52,46 @@ export default{
 .w1{
     width: 80%;
     height: 30px;
-    position: absolute;
+    position: relative;
     margin-top: 20px;
     left: 10%;
     text-align: left;
     line-height: 30px;
     font-size: 18px;
     border-bottom: 1px solid #ccc;
+}
+.w2{
+    width: 80%;
+    height: 24px;
+    position: relative;
+    left: 10%;
+    text-align: left;
+    line-height: 24px;
+    font-size: 14px;
+}
+.w3{
+    width: 80%;
+    height: 24px;
+    position: relative;
+    left: 10%;
+    text-align: center;
+    line-height: 24px;
+    font-size: 14px;
+}
+.w3 div{
+    position: absolute;
+    top: 0;
+    width: 14%;
+    height: 24px;
+    cursor: pointer;
+}
+.day{
+    left: 0;
+}
+.week{
+    left: 14%;
+}
+.month{
+    left: 28%;
 }
 </style>
