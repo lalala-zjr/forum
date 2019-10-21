344bdcc3a5f24d27d8702343ddaab68e80671e84
lalala-zjr <1365351516@qq.com>
2019-10-21 15:53:46 +0800

    首页

--- forum/src/Pages/main/person/person.vue
+++ forum/src/Pages/main/person/person.vue
@@ -1,7 +1,69 @@
 <template>
    <div id="person">
-       <p>&emsp;&emsp;&emsp;&emsp;&emsp;个人信息</p>
-       <div class="peronShow"></div>
+       <p class="inf">&emsp;&emsp;&emsp;&emsp;&emsp;个人信息</p>
+       <div class="peronShow">
+         <div class="user">
+           <p class="Pword">用户名</p>
+           <input type="text" placeholder="输入内容" class="Pinput" value="可怜且轩轩">
+         </div>
+         <div class="userRea">
+           <p class="Pword">真实姓名</p>
+           <input type="text" placeholder="输入内容" class="Pinput" value="张金蕊">
+           <select name="" id="">
+             <option value="1">公开</option>
+             <option value="2">保密</option>
+           </select>
+         </div>
+         <div class="phone">
+           <p class="Pword">手机号</p>
+           <input type="text" placeholder="输入内容" class="Pinput" value="18009240559">
+           <select name="" id="">
+             <option value="1">公开</option>
+             <option value="2">保密</option>
+           </select>
+         </div>
+         <div class="Birth">
+           <p class="Pword">生日</p>
+           <input type="text" placeholder="输入内容" class="Pinput" value="10.22">
+           <select name="" id="">
+             <option value="1">公开</option>
+             <option value="2">保密</option>
+           </select>
+         </div>
+         <div class="college">
+           <p class="Pword">学院</p>
+           <input type="text" placeholder="输入内容" class="Pinput" value="理学院">
+           <select name="" id="">
+             <option value="1">公开</option>
+             <option value="2">保密</option>
+           </select>
+         </div>
+         <div class="major">
+           <p class="Pword">专业</p>
+           <input type="text" placeholder="输入内容" class="Pinput" value="信息与计算科学">
+           <select name="" id="">
+             <option value="1">公开</option>
+             <option value="2">保密</option>
+           </select>
+         </div>
+         <div class="emil">
+           <p class="Pword">邮箱</p>
+           <input type="text" placeholder="输入内容" class="Pinput" value="1365351516@qq.com">
+           <select name="" id="">
+             <option value="1">公开</option>
+             <option value="2">保密</option>
+           </select>
+         </div>
+         <div class="like">
+           <p class="Pword">兴趣</p>
+           <input type="text" placeholder="输入内容" class="Pinput" value="Coding">
+           <select name="" id="">
+             <option value="1">公开</option>
+             <option value="2">保密</option>
+           </select>
+         </div>
+         <button class="save">保存</button>
+       </div>
    </div>
 </template>
 <script>
@@ -17,7 +79,7 @@ export default{
     width: 100%;
     height: 750px;
 }
-p{
+.inf{
     width: 100%;
     height: 50px;
     line-height: 75px;
@@ -30,5 +92,78 @@ p{
     position: relative;
     left: 15%;
     background-color: white;
+}
+.user,.userRea,.phone,.Birth,.college,.major,.emil,.like{
+  position: relative;
+  width: 60%;
+  height: 30px;
+  left: 20px;
+  /* background-color: skyblue; */
+}
+.user{
+  top: 5%;
+}
+.userRea{
+  top: 10%;
+}
+.phone{
+  top: 15%;
+}
+.Birth{
+  top: 20%;
+}
+.college{
+  top: 25%;
+}
+.major{
+  top: 30%;
+}
+.emil{
+  top: 35%;
+}
+.like{
+  top: 40%;
+}
+.Pword{
+  width: 16%;
+  height: 30px;
+  /* background-color: red; */
+  left: 4%;
+  margin: 0;
+  position: absolute;
+  font-size: 16px;
+  line-height: 30px;
+  text-align: left;
+}
+.Pinput{
+  box-sizing: border-box;
+  padding: 2% 5px;
+  position: absolute;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 50%;
+  height: 20px;
+  left: 20%;
+  top: 5px;
+}
+.save{
+  position: absolute;
+  bottom: 10%;
+  left: 50%;
+  width: 100px;
+  height: 40px;
+  margin-left: -40px;
+  background-color: #005fbc;
+  border: 1px solid #cccccc;
+  color: white;
+  border-radius: 10px;
+}
+select{
+  width: 15%;
+  height: 20px;
+  position: absolute;
+  top: 5px;
+  right: -40%;
+  text-align: center;
 }
 </style>
