b12ea0850d3f100c8696d52eae92624067f03619
lalala-zjr <1365351516@qq.com>
2019-10-22 17:03:21 +0800

    首页

---
+++ forum/src/Pages/main/detail/detail.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="detail">
+        <div>123</div>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+#detail{
+    background-color: #fff;
+    height: 730px;
+    width: 55%;
+    margin-left: 10%;
+    position: relative;
+    top: 30px;
+    bottom: 10px;
+}
+</style>
