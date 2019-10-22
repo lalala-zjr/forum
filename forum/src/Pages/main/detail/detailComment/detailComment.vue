a1ad033f79c29f84ce533da41b67a37792562df1
lalala-zjr <1365351516@qq.com>
2019-10-22 23:48:02 +0800

    首页

--- forum/src/Pages/main/detail/detailComment/detailComment.vue
+++ forum/src/Pages/main/detail/detailComment/detailComment.vue
@@ -1,5 +1,11 @@
 <template>
     <div id="detailComment">
+        <div class="head">
+            <img src="../../../../assets/img/user.png" alt="">
+            <div class="headword">扬扬叉会腰</div>
+        </div>
+        <div class="body">写的写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错写的真不错真不错</div>
+        <div class="foot"></div>
     </div>
 </template>
 <script>
@@ -13,7 +19,43 @@ export default {
 <style scoped>
 #detailComment{
     width: 100%;
-    height: 100px;
+    /* height: 120px; */
     border-bottom: 1px solid #ccc;
+}
+.head{
+    width: 100%;
+    height: 40px;
+    /* background-color: red; */
+    position: relative;
+    line-height: 40px;
+}
+img{
+    width: 6%;
+    height: 30px;
+    position: absolute;
+    left: 2%;
+    top: 5px;
+}
+.headword{
+    width: 88%;
+    height: 40px;
+    position: absolute;
+    right: 0;
+    top: 0;
+}
+.body{
+    width: 88%;
+    position: relative;
+    font-size: 14px;
+    top: 0;
+    left: 12%;
+}
+.foot{
+    width: 100%;
+    height: 40px;
+    position: relative;
+    bottom: 0;
+    left: 0;
+    /* background-color: red; */
 }
 </style>
