f02ca1bcfb086b23f76d7b68a8b2cb65b185fec5
lalala-zjr <1365351516@qq.com>
2019-10-23 17:53:04 +0800

    首页

--- forum/src/Pages/main/send/send.vue
+++ forum/src/Pages/main/send/send.vue
@@ -1,11 +1,57 @@
 <template>
-    <div id="send"></div>
+    <div id="send">
+        <editor id="tinymce" v-model="value" :init="init"></editor>
+    </div>
 </template>
 <script>
+import tinymce from 'tinymce/tinymce'
+import 'tinymce/themes/silver/theme'
+import Editor from '@tinymce/tinymce-vue'
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/media'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/contextmenu'
+import 'tinymce/plugins/wordcount'
+import 'tinymce/plugins/colorpicker'
+import 'tinymce/plugins/textcolor'
 export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    plugins: {
+      type: [String, Array],
+      default: 'lists image media table textcolor wordcount contextmenu'
+    },
+    toolbar: {
+      type: [String, Array],
+      default: 'undo redo |  formatselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | lists image media table | removeformat'
+    }
+  },
   data () {
     return {
+      init: {
+        language_url: '/static/tinymce/langs/zh_CN.js',
+        language: 'zh_CN',
+        skin_url: '/static/tinymce/skins/lightgray',
+        height: 300,
+        branding: false,
+        menubar: false
+      }
     }
+  },
+  components: {
+    Editor,
+    tinymce
+  },
+  mounted () {
+    tinymce.init({})
   }
 }
 </script>
@@ -15,6 +61,15 @@ export default {
     height: 700px;
     margin-left: 10%;
     margin-top: 30px;
-    background-color: white;
+    /* background-color: white; */
 }
+textarea{
+    display: block;
+}
+/* #tinymce{
+    width: 100%;
+    height: 500px;
+    visibility: visible;
+    z-index: 15;
+} */
 </style>
